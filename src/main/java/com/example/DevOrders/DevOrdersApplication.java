7ad0f1b2312bfa23b91c0f76f728ba9551472d3f
Irish Manalang <irish.r.manalang.mil@army.mil>
2024-10-28 12:19:15 -0500

    adding all starter files

---
+++ src/main/java/com/example/DevOrders/DevOrdersApplication.java
@@ -0,0 +1,13 @@
+package com.example.DevOrders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOrdersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevOrdersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DevOrders/DevOrdersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DevOrders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevOrdersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
