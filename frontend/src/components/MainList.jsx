1eb0e500d1812deb5b66e91329737fc6dc6cce2a
Irish Manalang <irish.r.manalang.mil@army.mil>
2024-10-30 17:08:54 -0500

    added table to List

--- frontend/src/components/MainList.jsx
+++ frontend/src/components/MainList.jsx
@@ -1,23 +1,61 @@
+import {Box, Button, Checkbox, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography} from "@mui/material";
+import React from "react";
+
 const MainList = () => {
+
+    const tasks = [
+        {
+            id: 1,
+            description: "Task 1",
+            taskComplete: true
+        },
+        {
+            id: 2,
+            description: "Task 2",
+            taskComplete: false
+        }
+    ]
+
     return (
-        <div>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Email</th>
-                    </tr>
-                </thead>
 
-                <tbody>
-                    <tr>
-                        <td>Name</td>
-                        <td>Email1</td>
-                    </tr>
-                </tbody>
+        <Box>
 
-            </table>
-        </div>
+            <Typography variant="h4">List</Typography>
+
+            <TableContainer component={Paper}>
+
+                <Table>
+
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Completion Status</TableCell>
+                            <TableCell>Description</TableCell>
+                            <TableCell>Actions</TableCell>
+                        </TableRow>
+                    </TableHead>
+
+                    <TableBody>
+                        {tasks.map((task) => (
+                            <TableRow key={task.id} className="table-row">
+                                <TableCell>
+                                    <Checkbox checked={task.taskComplete} />
+                                </TableCell>
+                                <TableCell>{task.description}</TableCell>
+                                <TableCell>
+                                    <Button>Edit</Button>
+                                    <Button>Delete</Button>
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+
+                </Table>
+
+            </TableContainer>
+
+        </Box>
+
+
     );
 };
 
