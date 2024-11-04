8e48a39bdb7fdd57b545a730ee46594437363bf4
Irish Manalang <irish.r.manalang.mil@army.mil>
2024-11-04 08:34:50 -0600

    added Reousrces page markdown framework

---
+++ frontend/public/ResourcesDoc.md
@@ -0,0 +1,23 @@
+# Resources
+
+## Table of Contents
+1. [Introduction](#introduction)
+2. [Features](#features)
+3. [Installation](#installation)
+
+## Introduction
+Briefly describe your project. Explain what it does, who it’s for, and any other relevant details.
+
+## Features
+- **Feature 1**: Short description
+- **Feature 2**: Short description
+- **Feature 3**: Short description
+
+## Installation
+### Prerequisites
+List any prerequisites for the project (e.g., Node.js, Java, Python, etc.).
+
+### Steps
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/username/project-name.git
--- frontend/src/components/ResourcesPage/ResourcesPage.jsx
+++ frontend/src/components/ResourcesPage/ResourcesPage.jsx
@@ -1,8 +1,23 @@
+import ReactMarkdown from "react-markdown";
+import {useEffect, useState} from "react";
+import {Box} from "@mui/material";
+import './ResourcePage.css'
+
+
 const ResourcesPage = () => {
+
+    const [markdownContent, setMarkdownContent] = useState('');
+
+    useEffect(() => {
+        fetch('../../../public/ResourcesDoc.md')
+            .then((res) => res.text())
+            .then((text) => setMarkdownContent(text))
+    }, []);
+
     return (
-        <div>
-            resources
-        </div>
+        <Box className="page-container">
+            <ReactMarkdown className='markdown-body'>{markdownContent}</ReactMarkdown>
+        </Box>
     );
 };
 
